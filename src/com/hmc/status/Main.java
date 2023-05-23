98f75449342733eeec9651ca253975215456cd1a
Leslie林 <85997713+LhbCss@users.noreply.github.com>
2023-05-23 16:04:26 +0800

    Update Main.java

--- src/com/hmc/status/Main.java
+++ src/com/hmc/status/Main.java
@@ -5,19 +5,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.hmc.status.commands.HMC_Command;
-
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		
-		Bukkit.getPluginCommand("hmc").setExecutor(new HMC_Command());
-		System.out.println("HMC Status已启动!");
+	
+		System.out.println("测试启动成功");
 		
 	}
 	@Override
 	public void onDisable() {
-		//System.out.println("HMC Status已卸载!");
+		System.out.println("测试卸载成功！");
 	}
 	
 	
